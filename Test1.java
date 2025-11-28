d366466ed0013b30f7e9935be06ae331662201e4
arun <arunachalamselvam9965@gmail.com>
2025-11-28 23:58:36 +0530

    commit

--- Test1.java
+++ Test1.java
@@ -2,6 +2,6 @@ package GitLearning2;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("This is Test1 class and it has been modified.");
+        System.out.println("This is Test1 class and it has been modified and saved.");
     }
 }
